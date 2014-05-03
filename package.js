f1aa281284233f61306e9839e6b1efb173bd5f99
Brent Anderson <me@brentjanderson.com>
2014-05-03 17:21:03 -0600

    Revision - submodules unfortunately don't work when the source file does not export properly.

---
+++ package.js
@@ -0,0 +1,8 @@
+Package.describe({
+  summary: "Three.js - A JavaScript 3D library"
+});
+
+Package.on_use(function (api) {
+  api.add_files("lib/three.js", "client");
+  api.export('THREE','client');
+});
