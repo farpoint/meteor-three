3cf32cf9ab48e3ba570cee6b3fcdbf897944788d
Alex <alexanderson1993@gmail.com>
2014-05-03 18:00:59 -0600

    Added OBJ & MTL Loader

--- package.js
+++ package.js
@@ -4,5 +4,8 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.add_files("lib/three.js", "client");
+    api.add_files('lib/OBJMTLLoader.js', 'client');
+    api.add_files('lib/MTLLoader.js', 'client');
+    api.add_files('lib/OBJLoader.js', 'client');
   api.export('THREE', 'client');
 });
